8d78b82a402eb0201adf5bea4b7b75dc6e7e4b78
yogita <yogita@leegality.com>
2023-03-27 15:56:26 +0530

    update db json location

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const path = require('path')
 
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(path.join(`${__dirname}/tmp/`, 'db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(cors())
